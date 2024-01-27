a4fbe5d5672b65ce3b5732d191af486fee939dae
Ahmed Sabry <104426964+ahmed50502022@users.noreply.github.com>
2024-01-28 00:49:43 +0200

    Add files via upload

---
+++ Pizza resturant project.sql
@@ -0,0 +1,52 @@
+-- 1. Total Revenues
+
+SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
+
+-- 2. Average Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza_sales
+
+-- 3. Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales
+
+-- 4. Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales
+
+-- 5. Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza_sales
+
+-- 6. Daily Trend for Total Orders
+SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date)
+
+-- 7. Monthly Trend for Orders
+select DATENAME(MONTH, order_date) as Month_Name, COUNT(DISTINCT order_id) as Total_Orders
+from pizza_sales
+GROUP BY DATENAME(MONTH, order_date)
+
+-- 8. Percentage of sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+-- 9. Top 5 Pizzas by Revenue
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC
+
+-- 10. Bottom 5 Pizzas by Revenue
+SELECT Top 5 pizza_name, SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue ASC
+
+
+
+
+
+
